d08d6b34d71eb6fa59d344b8047e3eebf5fa8485
Julien <jbertazzolambert@gmail.com>
2021-12-25 14:16:17 -0500

    basic stuff

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react'
 
 export default function App() {
+  const [total, setTotal] = useState(5)
+  const [amount, setAmount] = useState("")
+  const [description, setDescription] = useState("")
+
+  const changeAmount = (amount) => {
+    amount = amount.replace(/[^0-9]/g, '')
+    setAmount(amount)
+  }
+
+  const addAmount = () => {
+    setTotal(prevTotal => prevTotal + parseInt(amount))
+    setAmount("")
+    setDescription("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.totalContainer}>
+        <Text style={styles.label}>Total Saved:</Text>
+        <Text style={styles.total}>{total}</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput placeholder="amount" value={amount} onChangeText={changeAmount} keyboardType="numeric" />
+        <TextInput placeholder="description" value={description} onChangeText={setDescription} />
+        <Button title="+" onPress={addAmount} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +40,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+  },
+  totalContainer: {
+    width: '100%',
+  },
+  label: {
+    fontSize: 20,
+    textAlign: 'left',
+    width: '100%',
+    fontWeight: '500',
+    padding: 8,
+    color: 'ivory',
+    backgroundColor: 'gray',
+  },
+  total: {
+    color: 'ivory',
+    fontFamily: 'monospace',
+    backgroundColor: 'darkslategray',
+    fontSize: 32,
+    padding: 8,
   },
 });
